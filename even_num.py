e7e5ddfd1820a1a9be094b640139531f285eacd6
snehakantule <snehakantule332@gmail.com>
2023-05-31 17:42:11 +0530

    new created

---
+++ even_num.py
@@ -0,0 +1,5 @@
+def even_num():
+    for i in range(31,51):
+        if i%2==0:
+            print(i,end=" ")
+even_num()
---
+++ odd_num.py
@@ -0,0 +1,6 @@
+def odd_num():
+    for i in range(21,51):
+        if i%2!=0:
+            print(i,end=" ")
+odd_num()
+
